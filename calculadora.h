07899f4166b0bcc444c113425ebb8131471de283
Reh <142178373+rehoaguiar@users.noreply.github.com>
2024-06-17 19:31:42 -0300

    Update calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -1,11 +1,7 @@
 #ifndef CALCULADORA_H
 #define CALCULADORA_H
 
-#include <math.h>
-
-#define MAX_EXPR_LEN 100
 #define MAX_PILHA_TAM 100
-#define DEG_TO_RAD(deg) ((deg) * M_PI / 180.0)
 
 typedef struct {
     double num[MAX_PILHA_TAM];
@@ -17,15 +13,9 @@ typedef struct {
     int topo;
 } ArmazenarStrings;
 
-typedef struct {
-    char posFixa[512];     // Expressão na forma pós-fixada, como 3 12 4 + *
-    char inFixa[512];      // Expressão na forma infixada, como 3 * (12 + 4)
-    float Valor;           // Valor numérico da expressão  
-} Expressao;
-
+// Declarações das funções
 void adicionar(ArmazenarNumeros* armazenarNumeros, double valor);
 double remover(ArmazenarNumeros* armazenarNumeros);
-
 void adicionarString(ArmazenarStrings* armazenarStrings, char* valor);
 char* removerString(ArmazenarStrings* armazenarStrings);
 
@@ -42,8 +32,6 @@ double logaritmo(double a);
 
 double avaliarPosfixa(char* expr);
 void posfixaParaPreFixa(char* expr, char* resultado);
+void posfixaParaInfixa(char* expr, char* resultado);
 
-char* getFormaInFixa(char* Str);    // Retorna a forma infixada de Str (pós-fixada)
-float getValor(char* Str);          // Calcula o valor de Str (na forma pós-fixada)
-
-#endif // CALCULADORA_H
+#endif
